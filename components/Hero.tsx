1969b0e6d4613418a4758e8367061e6d2f4faa91
Hassan <hassanajibade17@gmail.com>
2024-11-10 01:43:08 +0100

    doing one or two

---
+++ components/Hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+<section className='border-blue-500 flex-col cursor-pointer-500 border-2 flex w-full h-max py-10 lg:py-20 xl:flex-row  gap-20 pb-32 md:gap-28 '>
+  <div className='hero-map'/>
+</section>
+  )
+}
+
+export default Hero
